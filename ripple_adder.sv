22fb6341b149509c88dc2a918631e01ef1b25440
Chavez, Jorge <jorgejc2@illinois.edu>
2021-09-12 22:46:28 -0500

    finishsed ripple adders

--- ripple_adder.sv
+++ ripple_adder.sv
@@ -11,6 +11,13 @@ module ripple_adder
      * Insert code here to implement a ripple adder.
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
-
+	  
+	  logic c0, c1, c2;
+	  
+	  //(4bit_rp) inputs: A[3:0] B[3:0] cin outputs: S C
+	  4bit_rp rp1 (.A(A[3:0]), .B(B[3:0]), .cin(cin), .S(S[3:0]), .C(C0);
+	  4bit_rp rp1 (.A(A[7:4]), .B(B[7:4]), .cin(c0), .S(S[7:4]), .C(C1);
+	  4bit_rp rp1 (.A(A[11:8]), .B(B[11:8]), .cin(c1), .S(S[7:4]), .C(C2);
+	  4bit_rp rp1 (.A(A[15:12]), .B(B[15:12]), .cin(c2), .S(S[3:0]), .C(cout);
      
 endmodule
