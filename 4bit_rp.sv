8690a0634cf3d8015af842612a986b1e300214a6
ssbonde2 <ssbonde2@illinois.edu>
2021-09-15 00:34:17 -0700

    changes

--- 4bit_rp.sv
+++ 4bit_rp.sv
@@ -7,9 +7,9 @@ module fourbit_rp
 	 logic c0, c1, c2;
 	 
 	 //(full_adder) inputs: .A, .B, .cin outputs: .S, .C
-	 full_adder f1 = (.A(A[0]), .B(B[0], .Cin(cin), .S(S[0]), .C(c0));
-	 full_adder f2 = (.A(A[1]), .B(B[1], .Cin(co), .S(S[1]), .C(c1));
-	 full_adder f3 = (.A(A[2]), .B(B[2], .Cin(c1), .S(S[2]), .C(c2));
-	 full_adder f4 = (.A(A[3]), .B(B[3], .Cin(c2), .S(S[3]), .C(c3));
+	 full_adder f1 (.A(A[0]), .B(B[0], .Cin(cin), .S(S[0]), .C(c0));
+	 full_adder f2 (.A(A[1]), .B(B[1], .Cin(co), .S(S[1]), .C(c1));
+	 full_adder f3 (.A(A[2]), .B(B[2], .Cin(c1), .S(S[2]), .C(c2));
+	 full_adder f4 (.A(A[3]), .B(B[3], .Cin(c2), .S(S[3]), .C(c3));
 	 
 endmodule 
---
+++ testbench.sv
@@ -0,0 +1,28 @@
+module testbench();
+
+	timeunit 10ns;
+	timeprecision 1ns;
+	
+	logic [15:0] A;
+	logic [15:0] B;
+	logic [15:0] Sum_RC;
+	logic CO_RC;
+	
+	
+	ripple_adder 				 RA(.A(A), .B(B), .Sum(Sum_RC),  .CO(CO_RC));
+		
+	//Begin the testing
+	initial begin
+	
+	
+		A = 16'h3333;
+		B = 16'h4444;
+		
+		#30
+		A = 16'h1111;
+		B = 16'h2222;
+		
+	end
+	
+
+endmodule 
