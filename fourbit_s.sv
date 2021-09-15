a25306080f6394cf3ed6025f23542a9c9a71e480
ssbonde2 <ssbonde2@illinois.edu>
2021-09-15 02:56:13 -0700

    select adder done

--- fourbit_s.sv
+++ fourbit_s.sv
@@ -1,1 +1,20 @@
-module fourbit_s()
+module fourbit_s(
+	input logic [3:0] A,
+	input logic [3:0] B,
+	input logic Cin,
+	output logic [3:0] Sum,
+	output logic Cout
+);
+
+
+	logic [3:0] S_high;
+	logic [3:0] S_low;
+	logic C_high, C_low;
+
+	fourbit_rp CarryHigh(.A(A), .B(B), .Cin(1'b1), .S(S_high), .Cout(C_high));
+	fourbit_rp CarryLow(.A(A), .B(B), .Cin(1'b0), .S(S_low), .Cout(C_low));
+	
+	two_to_one_mux outMux(S_high, S_low, Cin, Sum);
+	assign Cout = (Cin & C_high) | C_low;
+	
+endmodule
