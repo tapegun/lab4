f6f86d28e235280b930da9324f2831aa78c5b714
ssbonde2 <ssbonde2@illinois.edu>
2021-09-16 23:43:26 -0500

    select adder done?

--- fourbit_s.sv
+++ fourbit_s.sv
@@ -11,8 +11,8 @@ module fourbit_s(
 	logic [3:0] S_low;
 	logic C_high, C_low;
 
-	fourbit_rp CarryHigh(.A(A), .B(B), .Cin(1'b1), .S(S_high), .Cout(C_high));
-	fourbit_rp CarryLow(.A(A), .B(B), .Cin(1'b0), .S(S_low), .Cout(C_low));
+	fourbit_rp CarryHigh (.A(A), .B(B), .cin(1'b1), .S(S_high), .cout(C_high));
+	fourbit_rp CarryLow (.A(A), .B(B), .cin(1'b0), .S(S_low), .cout(C_low));
 	
 	two_to_one_mux outMux(S_high, S_low, Cin, Sum);
 	assign Cout = (Cin & C_high) | C_low;
--- select_adder.sv
+++ select_adder.sv
@@ -12,9 +12,9 @@ module select_adder (
      * Feel free to create sub-modules or other files. */
 	  
 	logic [2:0] C;
-	fourbit_s SA0(.A(A[3:0]), .B(B[3:0]), .Cin(1'b0), .Cout(C[0]), .S(Sum[3:0]));
-	fourbit_s SA1(.A(A[7:4]), .B(B[7:4]), .Cin(C[0]), .Cout(C[1]), .S(Sum[7:4]));
-	fourbit_s SA2(.A(A[11:8]), .B(B[11:8]), .Cin(C[1]), .Cout(C[2]), .S(Sum[11:8]));
-	fourbit_s SA3(.A(A[15:12]), .B(B[15:12]), .Cin(C[2]), .Cout(CO), .S(Sum[15:12]));
+	fourbit_s SA0(.A(A[3:0]), .B(B[3:0]), .Cin(1'b0), .Cout(C[0]), .Sum(Sum[3:0]));
+	fourbit_s SA1(.A(A[7:4]), .B(B[7:4]), .Cin(C[0]), .Cout(C[1]), .Sum(Sum[7:4]));
+	fourbit_s SA2(.A(A[11:8]), .B(B[11:8]), .Cin(C[1]), .Cout(C[2]), .Sum(Sum[11:8]));
+	fourbit_s SA3(.A(A[15:12]), .B(B[15:12]), .Cin(C[2]), .Cout(cout), .Sum(Sum[15:12]));
 
 endmodule
